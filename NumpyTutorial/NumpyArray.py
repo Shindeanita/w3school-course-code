96386be5c05d0a7214e14eecc3e23c85cffe78ff
Anita Shinde <108342562+Shindeanita@users.noreply.github.com>
2022-11-30 15:50:14 +0530

    Numpy Tutorial Added

---
+++ NumpyTutorial/NumpyArray.py
@@ -0,0 +1,15 @@
+import numpy as np
+print("Used a array to create a NumPy array:")
+a = np.array([2,5,87,8,6])
+print(a)
+print(type(a))
+
+#Version of numpy
+
+print(np.__version__)
+
+
+#Use a tuple to create a NumPy array:
+print("Used a tuple to create a NumPy array:")
+b = np.array((1,2,5,6,'a','b','c'))
+print(b)
